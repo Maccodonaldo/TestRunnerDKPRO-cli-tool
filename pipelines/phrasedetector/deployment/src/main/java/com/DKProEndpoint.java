4d8aa6dcfdbc49a27e9e192088584f104e88f4eb
Daniel Hoppe Alvarez <dhoppealvarez@gmail.com>
2019-03-07 13:57:44 +0100

    Helper classes and one example

---
+++ pipelines/phrasedetector/deployment/src/main/java/com/DKProEndpoint.java
@@ -0,0 +1,87 @@
+package com;
+// @DKPRO CLI import code generation is starting this line
+
+import de.unidue.phrasedetector.PhraseAnnotationPipeline;
+
+
+
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.TypeSystem;
+import org.apache.uima.cas.impl.XmiCasSerializer;
+import org.apache.uima.jcas.JCas;
+
+import org.apache.uima.util.XMLSerializer;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.ByteArrayOutputStream;
+
+@RestController
+public class DKProEndpoint {
+    // @DKPRO CLI init static analysis, starting this line
+
+	public static PhraseAnnotationPipeline analysis = new PhraseAnnotationPipeline();
+        
+    @RequestMapping(
+            value = "/analysis", method = RequestMethod.POST, consumes = "text/plain")
+    public static String analyzeText(@RequestBody String jsonString) throws Exception {
+
+        try {
+            
+            // @DKPRO CLI analysis code generation is starting this line
+
+			JCas result = analysis.run(jsonString);
+            
+
+            return JCasToXMIString(result);
+
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            return "Hat nicht funktionukkelt";
+        }
+
+
+    }
+
+    private static String JCasToXMIString(JCas result) throws Exception {
+
+        ByteArrayOutputStream outStream = null;
+
+        try {
+            // create out stream
+            outStream = new ByteArrayOutputStream();
+            XMLSerializer xmlSer = new XMLSerializer(outStream, false);
+            // create cas from jcas (result)
+            CAS resultCas = result.getCas();
+
+            // get current cas type system
+            TypeSystem resultType = resultCas.getTypeSystem();
+
+            // set up CAS serializer with type system
+            XmiCasSerializer xmi_cas = new XmiCasSerializer(resultType);
+
+
+            xmi_cas.serialize(resultCas, xmlSer.getContentHandler());
+
+            String resultXMLString = outStream.toString();
+
+            return resultXMLString;
+
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            return "JCas to String hat nicht funktioniert";
+
+        } finally {
+
+            if (outStream != null) {
+                outStream.close();
+            }
+        }
+
+    }
+
+}
+
